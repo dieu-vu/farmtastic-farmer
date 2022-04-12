edeb324592b192f579d624e4a3a9ff923bd289c4
HangHuynh19 <48004616+HangHuynh19@users.noreply.github.com>
2022-04-12 19:50:50 +0300

    UI fix in change password half sheet

--- FarmtasticFarmer/Profile/UI/UIComponents/ChangePasswordView.swift
+++ FarmtasticFarmer/Profile/UI/UIComponents/ChangePasswordView.swift
@@ -15,40 +15,41 @@ struct ChangePasswordView: View {
     var body: some View {
         NavigationView {
             VStack {
+                Text("Change Password").font(.title).bold()
                 VStack(alignment: .leading, spacing: 15) {
-                    Text("Change Password").font(.title).bold().padding(32)
                     TextField("New password", text: $changePasswordControler.password)
                         .padding()
                         .overlay(
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(Color.gray, lineWidth: 1)
                         )
-                        .disabled(isDisabled)
+                        //.disabled(isDisabled)
                     TextField("Confirmed password", text: $changePasswordControler.confirmedPassword)
                         .padding()
                         .overlay(
                             RoundedRectangle(cornerRadius: 8)
                                 .stroke(Color.gray, lineWidth: 1)
                         )
-                        .disabled(isDisabled)
-                }.padding([.leading, .trailing], 27.5)
+                        //.disabled(isDisabled)
+                }
+                .padding([.leading, .trailing], 27.5)
                 ButtonView(buttonText: "Apply", buttonColorLight: "LightGreen", buttonColorDark: "DarkGreen" , buttonAction: {
-                    closeKeyboard()
-                    isDisabled = true
-                    showChangePassword.toggle()
+                    //closeKeyboard()
+                    //isDisabled = true
                     changePasswordControler.changePassword()
+                    showChangePassword.toggle()
                 })
                     .padding(.bottom, 32)
             }
         }
     }
-    
-    func closeKeyboard() {
-        UIApplication.shared.sendAction(
-            #selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil
-        )
-    }
 }
+
+/*func closeKeyboard() {
+    UIApplication.shared.sendAction(
+        #selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil
+    )
+}*/
 
 struct ChangePasswordView_Previews: PreviewProvider {
     static var previews: some View {
